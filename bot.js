e35b5937d2e9b5570cf662ec305a78976c8abd9d
Yousef909909 <67248158+Yousef909909@users.noreply.github.com>
2020-06-21 19:30:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['Your ID']
+const myid = ['598826008128520207']
 const client = new Discord.Client();
 const client2 = new Discord.Client();
 const client3 = new Discord.Client();
